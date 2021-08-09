31a07287a0302b3a351428b53c2d7957fbc9ee05
Anas <amanabbasi3660@gmail.com>
2021-08-09 16:41:46 +0530

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 space-y-10 md:space-y-0 px-32 py-14 bg-gray-100">
+      <div>
+        <h5 className="space-y-4 text-xs text-gray-800">ABOUT</h5>
+        <p className="font-bold">How Airbnb works</p>
+        <p className="font-bold">Newsroom</p>
+        <p className="font-bold">Investors</p>
+        <p className="font-bold">Airbnb Plus</p>
+        <p className="font-bold">Airbnb Luxe</p>
+      </div>
+      <div>
+        <h5 className="space-y-4 text-xs text-gray-800">COMMUNITY</h5>
+        <p className="font-bold">Accessibility</p>
+        <p className="font-bold">This is not a real site</p>
+        <p className="font-bold">Its a pretty awesom clone</p>
+        <p className="font-bold">Refferal accepted</p>
+        <p className="font-bold">Next Clone</p>
+      </div>
+      <div>
+        <h5 className="space-y-4 text-xs text-gray-800">HOST</h5>
+        <p className="font-bold">Anas</p>
+        <p className="font-bold">Presents</p>
+        <p className="font-bold">Learn Tailwind</p>
+        <p className="font-bold">Hundreds of Student</p>
+        <p className="font-bold">Join Now</p>
+      </div>
+      <div>
+        <h5 className="space-y-4 text-xs text-gray-800">SUPPORT</h5>
+        <p className="font-bold">Help Center</p>
+        <p className="font-bold">Trust & Safety</p>
+        <p className="font-bold">Say Hi Youtube</p>
+        <p className="font-bold">Easter Eggs</p>
+        <p className="font-bold">For the Win</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
