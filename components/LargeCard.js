44286d0704e382f63312adad8e3e9589aa06a73d
Anas <amanabbasi3660@gmail.com>
2021-08-09 16:25:53 +0530

    add large card

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -11,7 +11,13 @@ function LargeCard({ img, title, description, buttonText }) {
           objectFit="cover"
         />
       </div>
-      <div></div>
+      <div className="absolute top-32 left-12">
+        <h3 className="text-4xl mb-3 w-64">{title}</h3>
+        <p>{description}</p>
+        <button className="text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5">
+          {buttonText}
+        </button>
+      </div>
     </section>
   );
 }
