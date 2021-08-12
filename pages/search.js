f5454f120c448ceafe89deef986c605825235180
Anas <amanabbasi3660@gmail.com>
2021-08-12 23:13:43 +0530

    add progress bar

--- pages/search.js
+++ pages/search.js
@@ -2,8 +2,9 @@ import { useRouter } from "next/dist/client/router";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { format } from "date-fns";
+import InfoCard from "../components/InfoCard";
 
-function Search() {
+function Search({ searchResults }) {
   const router = useRouter();
   const { location, noofGuest, startDate, endDate } = router.query;
 
@@ -24,12 +25,27 @@ function Search() {
           <h1 className="text-3xl font-semibold mt-2 mb-6 ">
             Stays in {location}
           </h1>
-          <div className="lg:inline-flex mt-3 mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+          <div className="hidden lg:inline-flex mt-3 mb-5 space-x-3 text-gray-800 whitespace-nowrap">
             <p className="button">Cancellation Flexibility</p>
             <p className="button">Type of Place</p>
             <p className="button">Price</p>
             <p className="button">Rooms and Beds</p>
             <p className="button">More Filters</p>
+          </div>
+
+          <div className="flex flex-col">
+            {searchResults?.map((item) => (
+              <InfoCard
+                key={item.img}
+                img={item.img}
+                location={item.location}
+                title={item.title}
+                star={item.star}
+                price={item.price}
+                total={item.total}
+                description={item.description}
+              />
+            ))}
           </div>
         </section>
       </main>
@@ -40,3 +56,14 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps() {
+  const searchResults = await fetch("https://links.papareact.com/isz").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      searchResults,
+    },
+  };
+}
